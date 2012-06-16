295e61f2106271ae6ae36d5c645f1352e77a9d04
Jeryn Mathew <jerynmathew@gmail.com>
2012-06-16 10:07:48 +0530

    Models and Persistant data
    Models have been moved to independant script file.
    Bookmarks are now stored as a simple JSON file.

--- main.py
+++ main.py
@@ -1,65 +1,36 @@
 from flask import Flask, render_template, flash, request
-import re, os
+import os
+import model
 
 app = Flask(__name__)
 app.debug = True # Remove before deploying
 app.secret_key = os.urandom(32) # Need this for flash messages
-
-BookmarksList = [] #Replace with actual sqlite DB
     
 '''
 Main Page of Web App
 '''
 @app.route('/', methods=['GET', 'POST'])
 def bookmarks():
-    global BookmarksList
-    #By default, display the Entire table
-    render_table = BookmarksList
-    
+    # By default, display the Entire table
+    render_table = model.getBookmarks()
     # POST METHOD handling
     if request.method == 'POST':
         #Adding new bookmarks, and display complete (new) table
         if "textLink" in request.form.keys():
-            addNewBookmark()
+            model.addBookmark( Title=request.form["textTitle"],
+                               Link=request.form["textLink"],
+                               Tags=request.form["textTags"] )
+            
+            render_table = model.getBookmarks()
         #Search Request. Display only seacrhed entries!!
         elif "search" in request.form.keys():
-            render_table = searchTags(request.form["search"])
+            render_table = model.searchBookmarks( request.form["search"] )
             # In the event u searched for a non-existent tag 
             if len( render_table ) == 0:
                 flash("No entries with the tag \'" + request.form["search"] + "\'")
     
     # Default GET METHOD Handling    
     return render_template( 'index.html', table=render_table, headers=['Title', 'Url', 'Tags'] )
-    
-
-'''
-Adds New Bookmark to the global list.
-TODO: Add entry into SQLite DB.
-'''    
-def addNewBookmark():
-    Bookmark = {}
-    
-    Bookmark["Title"] = request.form["textTitle"]
-    if request.form["textLink"].startswith( "http://") is False:
-        Bookmark["Link"]  = "http://" + request.form["textLink"]
-    else:
-        Bookmark["Link"]  = request.form["textLink"]
-    Bookmark["Tags"]  = re.findall( r'\w+', request.form["textTags"] )
-    
-    global BookmarksList
-    BookmarksList.append( Bookmark )
-    
-'''
-Search for tag
-'''    
-def searchTags( searchstring ):
-    table = []
-    global BookmarksList
-    for entry in BookmarksList:
-        print entry
-        if searchstring.lower() in [x.lower() for x in entry["Tags"]]:
-            table.append( entry )
-    return table
 
     
 # MAIN APPLICATION CODE
---
+++ model.py
@@ -0,0 +1,42 @@
+import json
+import re
+
+JSON_FILE = r".\static\bookmarks.json"
+
+'''
+Fetches bookmarks table
+'''
+def getBookmarks():
+    fp = open( JSON_FILE, 'r' )
+    table = json.load( fp )
+    fp.close()
+    return table
+
+'''
+Search Table of Bookmarks for specific tag
+'''
+def searchBookmarks( searchstring ):
+    table = []
+    fulltable = getBookmarks()
+    print fulltable
+    for entry in fulltable:
+        if searchstring.lower() in [ x.lower() for x in entry["Tags"] ]:
+            table.append( entry )
+    return table
+    
+'''
+Add new bookmark to file
+'''
+def addBookmark( **bookmark ):
+    table = getBookmarks()
+    
+    if bookmark["Link"].startswith( "http://") is False:
+        bookmark["Link"] = "http://" + bookmark["Link"]
+    bookmark["Tags"] = re.findall( r'\w+', bookmark["Tags"] )
+        
+    table.append( bookmark )
+    
+    fp = open( JSON_FILE, 'w' )
+    json.dump( table, fp )
+    fp.close()
+    
